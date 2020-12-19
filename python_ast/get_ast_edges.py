4856f1c86c1a911ef2362e02959fbc28ab652381
Harsh Parekh <harsh_parekh@outlook.com>
2020-12-19 18:54:19 -0500

    Simplify ast_visit and refactor construct_edges into generator function.

--- python_ast/get_ast_edges.py
+++ python_ast/get_ast_edges.py
@@ -1,6 +1,5 @@
 import ast
 import sys
-from collections import OrderedDict
 
 input_code_file = sys.argv[1]
 output_edges_file = sys.argv[2]
@@ -18,46 +17,38 @@ def node_kind(node):
     return node.__class__.__name__
 
 ast_nodes = []
-def ast_visit(node, level=0):
-    ast_nodes.append((node, level))
-    for field, value in ast.iter_fields(node):
+def ast_visit(node):
+    ast_nodes.append(node)
+    for _, value in ast.iter_fields(node):
         if isinstance(value, list):
             for item in value:
                 if isinstance(item, ast.AST):
-                    ast_visit(item, level=level+1)
+                    ast_visit(item)
         elif isinstance(value, ast.AST):
-            ast_visit(value, level=level+1)
+            ast_visit(value)
 
-def construct_edges():
-  edges = []
-  prev_node = ast_nodes[0][0]
-  prev_level = 0
 
-  for index in range(1, len(ast_nodes)):
-    node = ast_nodes[index][0]
-    level = ast_nodes[index][1]
-    
-    if level > prev_level:
-      edges.append((prev_node, node))
-      prev_node = node
-      prev_level = level
-    elif level <= prev_level:
-      for ind in reversed(range(index)):
-        if ast_nodes[ind][1] < level:
-          edges.append((ast_nodes[ind][0], node))
-          prev_node = node
-          prev_level = level
-          break
-
-  return edges
+def edges(node):
+    """
+    Yields all the edges in the ast sub-tree rooted at node.
+    """
+    for _, value in ast.iter_fields(node):
+        if isinstance(value, list):
+            for item in value:
+                if isinstance(item, ast.AST):
+                    yield (node, item)
+                    yield from edges(item)
+        elif isinstance(value, ast.AST):
+            yield (node, value)
+            yield from edges(value)
 
 
 with open(input_code_file, 'r') as infile:
-  code = infile.read()
-  ast_visit(ast.parse(code))
+  ast_root_node = ast.parse(infile.read())
+  ast_visit(ast_root_node)
 
-  node_ids = { node[0]: index for index, node in enumerate(ast_nodes) }
+  node_ids = { node: index for index, node in enumerate(ast_nodes) }
   
   with open(output_edges_file, 'w') as outfile:
-    for item in construct_edges():
-      outfile.write(f"{node_ids[item[0]]} {node_ids[item[1]]}\n")
+    for src, dest in edges(ast_root_node):
+      outfile.write(f"{node_ids[src]} {node_ids[dest]}\n")
