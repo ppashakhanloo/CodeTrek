495f2587e2374bcad330de45c69a843d26d91ebc
Harsh Parekh <harsh_parekh@outlook.com>
2020-12-19 23:59:01 -0500

    Lint with black.

--- python_ast/get_ast_edges.py
+++ python_ast/get_ast_edges.py
@@ -4,17 +4,23 @@ import sys
 input_code_file = sys.argv[1]
 output_edges_file = sys.argv[2]
 
+
 def node_str(node):
     if isinstance(node, ast.AST):
-        fields = (f"{name}={node_str(val)}" for name, val in ast.iter_fields(node) if name not in ('left', 'right'))
+        fields = (
+            f"{name}={node_str(val)}"
+            for name, val in ast.iter_fields(node)
+            if name not in ("left", "right")
+        )
         rv = f"{node.__class__.__name__}({', '.join(fields)})"
         return rv
     else:
         return repr(node)
 
+
 def node_kind(node):
-  if isinstance(node, ast.AST):
-    return node.__class__.__name__
+    if isinstance(node, ast.AST):
+        return node.__class__.__name__
 
 
 def nodes(node):
@@ -46,11 +52,11 @@ def edges(node):
             yield from edges(value)
 
 
-with open(input_code_file, 'r') as infile:
-  root_node = ast.parse(infile.read())
+with open(input_code_file, "r") as infile:
+    root_node = ast.parse(infile.read())
 
-  node_ids = { node: index for index, node in enumerate(nodes(root_node)) }
-  
-  with open(output_edges_file, 'w') as outfile:
-    for src, dest in edges(root_node):
-      outfile.write(f"{node_ids[src]} {node_ids[dest]}\n")
+    node_ids = {node: index for index, node in enumerate(nodes(root_node))}
+
+    with open(output_edges_file, "w") as outfile:
+        for src, dest in edges(root_node):
+            outfile.write(f"{node_ids[src]} {node_ids[dest]}\n")
