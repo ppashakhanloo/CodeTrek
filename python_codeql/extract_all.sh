c5bd806a9d1cc965650da39a245e807bf3213dfb
Aaditya Naik <aadi.naik@gmail.com>
2020-12-23 15:47:21 +0530

    Tried to parallelize the decode

--- python_codeql/extract_all.sh
+++ python_codeql/extract_all.sh
@@ -17,12 +17,15 @@ codeql database run-queries --threads="$threads" $db $dir
 # Generate CSV files
 bqrs_dir="$db/results"
 mkdir -p $out
+echo "Decoding all .bqrs files..."
 for path in $(find $bqrs_dir -name "*.bqrs")
 do
     f="$(basename $path)"
-    echo "Decoding for" $f
-    codeql bqrs decode --output="$out/${f%.*}".csv --format=csv --entities=id "$path"
+    # echo "Decoding for" $f
+   { codeql bqrs decode --output="$out/${f%.*}".csv --format=csv --entities=id "$path" && rm -rf path; } &
 done
-rm -rf $bqrs_dir
+echo "Waiting for all jobs to finish"
+wait
+# rm -rf $bqrs_dir
 echo "Done"
 
