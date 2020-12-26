7a272e9c9979b316da885cc514a2839a94ae321e
pardisp <ppashakhanloo@gmail.com>
2020-12-26 16:51:37 -0500

    add edges and labels

--- random_walk/random_walk.py
+++ random_walk/random_walk.py
@@ -3,17 +3,29 @@ import networkx as nx
 from pygraphviz import AGraph
 from random import choice
 
+# The output is a list of walks starting from the specified node. Each walk is
+# in this format: [(n1, n1_lbl), (n1, n2, e1_lbl), (n2, n2_lbl), ...]
+#                  _____^______  _______^_______   _____^_____
+#                     node            edge             node
+#
 def random_walk(graph, node, num_walks=100, num_steps=3,
                    include_nodes=None):
   walks = list()
   for walk in range(num_walks):
-    curr_walk = [node]
-    curr = node
+    curr_walk = [(node, graph.nodes[node]['label'])]
+    curr_node = node
+    curr_edge = None
     for step in range(num_steps):
-      neighbors = list(graph.neighbors(curr))
+      neighbors = list(graph.neighbors(curr_node))
       if len(neighbors) > 0:
-        curr = choice(neighbors)
-      curr_walk.append(curr)
+        prev_node = curr_node
+        curr_node = choice(neighbors)
+        curr_edge = (prev_node, curr_node,
+            choice(list(graph[prev_node][curr_node].values()))['label'])
+      
+      curr_walk.append(curr_edge)
+      curr_walk.append((curr_node, graph.nodes[curr_node]['label']))
+    
     # remove cyclic walks
     if not any(curr_walk.count(n) > 1 for n in curr_walk):
       # include nodes
@@ -34,4 +46,4 @@ if __name__ == '__main__':
   gv_file = sys.argv[1]
   graph = load_graph_from_gv(gv_file)
   for node in graph.nodes():
-    print(random_walk(graph, node, num_walks=10, num_steps=4, include_nodes=['n2']))
+    print(random_walk(graph, node, num_walks=10, num_steps=3))
