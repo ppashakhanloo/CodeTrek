a703fd61cd56fc3456c2e6fcbc32c6f7f3131e89
Yuepeng Wang <yuepeng.ypwang@gmail.com>
2020-12-27 12:04:08 -0600

    Type annotations for Datapoint

--- data_prep/datapoint.py
+++ data_prep/datapoint.py
@@ -1,43 +1,10 @@
-class DataPoint:
-    anchor = None      # type: TrajNode
-    trajectories = []  # type: List[Trajectory]
-    hints = []         # type: List[str]
-    label = None       # type: str
-
-    def __init__(self, anchor, trajectories, hints, label):
-        self.anchor = anchor
-        self.trajectories = trajectories
-        self.hints = hints
-        self.label = label
-
-    def to_dict(self) -> dict:
-        return {
-            'anchor': self.anchor.to_dict(),
-            'trajectories': [traj.to_dict() for traj in self.trajectories],
-            'hints': [hint for hint in self.hints],
-            'label': self.label
-        }
-
-
-class Trajectory:
-    nodes = []  # type: List[TrajNode]
-    edges = []  # type: List[TrajEdge]
-
-    def __init__(self, nodes, edges):
-        self.nodes = nodes
-        self.edges = edges
-
-    def to_dict(self) -> dict:
-        return {
-            'nodes': [node.to_dict() for node in self.nodes],
-            'edges': [edge.to_dict() for edge in self.edges]
-        }
+from typing import List, Dict
 
 
 class TrajNode:
     label = None  # type: str
 
-    def __init__(self, label):
+    def __init__(self, label: str):
         self.label = label
 
     def to_dict(self) -> str:
@@ -48,9 +15,45 @@ class TrajEdge:
     label1 = None  # type: str
     label2 = None  # type: str
 
-    def __init__(self, label1, label2):
+    def __init__(self, label1: str, label2: str):
         self.label1 = label1
         self.label2 = label2
 
     def to_dict(self) -> str:
         return '({l1},{l2})'.format(l1=self.label1, l2=self.label2)
+
+
+class Trajectory:
+    nodes = []  # type: List[TrajNode]
+    edges = []  # type: List[TrajEdge]
+
+    def __init__(self, nodes: List[TrajNode], edges: List[TrajEdge]):
+        self.nodes = nodes
+        self.edges = edges
+
+    def to_dict(self) -> Dict:
+        return {
+            'nodes': [node.to_dict() for node in self.nodes],
+            'edges': [edge.to_dict() for edge in self.edges]
+        }
+
+
+class DataPoint:
+    anchor = None      # type: TrajNode
+    trajectories = []  # type: List[Trajectory]
+    hints = []         # type: List[str]
+    label = None       # type: str
+
+    def __init__(self, anchor: TrajNode, trajectories: List[Trajectory], hints: List[str], label: str):
+        self.anchor = anchor
+        self.trajectories = trajectories
+        self.hints = hints
+        self.label = label
+
+    def to_dict(self) -> Dict:
+        return {
+            'anchor': self.anchor.to_dict(),
+            'trajectories': [traj.to_dict() for traj in self.trajectories],
+            'hints': [hint for hint in self.hints],
+            'label': self.label
+        }
