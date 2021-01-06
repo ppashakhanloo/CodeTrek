649aed51a376e62de55b1208b22e05b9cdeb2a13
Aaditya Naik <asnaik@seas.upenn.edu>
2021-01-06 10:31:45 -0500

    Combined db gen and edb dump in single script

--- datasets/cubert/not_cluster.sh
+++ datasets/cubert/not_cluster.sh
@@ -8,3 +8,10 @@ codeql database create ${bench}/codeqldb --language=python --source-root=${bench
 codeql query run -d ${bench}/codeqldb -o ${bench}/codeqldb/unused_local_vars.bqrs unusedlocalvar.ql > ${bench}/cluster_logs/runlog.out 2>${bench}/cluster_logs/runlog.err
 codeql bqrs decode -o ${bench}/codeqldb/unused_local_vars.csv --format=csv ${bench}/codeqldb/unused_local_vars.bqrs > ${bench}/cluster_logs/decodelog.out 2>${bench}/cluster_logs/decodelog.err
 codeql bqrs decode -o ${bench}/codeqldb/unused_local_vars_id.csv --entities=id --format=csv ${bench}/codeqldb/unused_local_vars.bqrs > ${bench}/cluster_logs/decodeidlog.out 2>${bench}/cluster_logs/decodeidlog.err
+rm -rf ${bench}/edb_dumps_whitelist
+mkdir -p ${bench}/edb_dumps_whitelist
+rm -rf ${bench}/results
+mkdir -p ${bench}/results
+mkdir -p ${bench}/cluster_logs
+rm -rf ${bench}/cluster_logs/dumplog.*
+../../python_codeql/extract_all.sh ../../python_codeql/python-edb-queries/whitelist-queries ${bench}/codeqldb ${bench}/edb_dumps_whitelist 8 > ${bench}/cluster_logs/dumplog.out 2>${bench}/cluster_logs/dumplog.err
