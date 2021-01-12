4c5489ccf70a7ceb9ec547b8e39b42598c6fbe16
Aaditya Naik <aadi.naik@gmail.com>
2021-01-12 13:21:16 -0500

    Added other python packages

--- install_pygraphviz.sh
+++ install_pygraphviz.sh
@@ -10,7 +10,8 @@ make install
 
 export PKG_CONFIG_PATH="$path/graphviz_bins/lib/pkgconfig/"
 pip install --global-option=build_ext --global-option="-I$path/graphviz_bins/include" --global-option="-L$path/graphviz_bins/lib" pygraphviz
-
+pip install graphviz
+pip install networkx
 p="$path/graphviz_bins/bin:$path/graphviz_bins/include:$path/graphviz_bins/lib:$path/graphviz_bins/share"
 
 echo -e "\nPATH=\"$p:\$PATH\"\nexport LD_LIBRARY_PATH=\"$p:\$LD_LIBRRARY_PATH\"\n" >> ~/.bashrc
