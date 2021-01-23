269ca220df7632f6d61d6dfe49b929d2e121d275
Aaditya Naik <aadi.naik@gmail.com>
2021-01-23 12:52:27 -0500

    Added script to get correct file

---
+++ datasets/cubert/get_correct_files.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+filename="$1"
+
+for i in $(cat final_batches/batch_7)
+do
+    num=$(basename $(dirname $i))
+    num=${num:5}
+    label=$(basename $(dirname $(dirname $i)))
+    base_dir=$(dirname $(dirname $(dirname $i)))
+    
+    if [[ "$label" == "misuse" ]]
+    then
+        echo $base_dir/correct/file_$((num-1))/source.py
+    else
+        echo $i
+    fi
+done
