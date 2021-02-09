1267d7b5ab357a02f7101f4a01a6117d5b8e5119
Yuepeng Wang <yuepeng.ypwang@gmail.com>
2021-02-08 20:26:15 -0600

    Add a script for shortest path

---
+++ dbwalk/rand_walk/graphutils.py
@@ -0,0 +1,28 @@
+import networkx as nx
+from pygraphviz import AGraph, Node
+from typing import List
+
+
+class GraphUtils:
+
+    @staticmethod
+    def load_graph_from_gv(path: str) -> AGraph:
+        graph = AGraph(path, directed=False)
+        return nx.nx_agraph.from_agraph(graph)
+
+    @staticmethod
+    def find_node_by_label(graph: AGraph, node_label: str) -> Node:
+        for node in graph.nodes():
+            if graph.nodes()[node]['label'] == node_label:
+                return node
+        raise NameError('Cannot find node with label:' + node_label)
+
+    @staticmethod
+    def find_label_by_id(graph: AGraph, id: str) -> str:
+        return graph.nodes()[id]['label']
+
+    @staticmethod
+    def shortest_path(graph: AGraph, from_label: str, to_label: str) -> List[str]:
+        from_node = GraphUtils.find_node_by_label(graph, from_label)
+        to_node = GraphUtils.find_node_by_label(graph, to_label)
+        return nx.shortest_path(graph, source=from_node, target=to_node)
