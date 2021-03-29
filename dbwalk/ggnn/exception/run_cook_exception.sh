2abec92dfba9cdf7205ef7d496c6eb510fd39236
Hanjun Dai <hadai@google.com>
2021-03-29 05:08:23 +0000

    code2seq + gnn process except one-func graphs

---
+++ dbwalk/ggnn/exception/run_cook_exception.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+data_root=$HOME/data/dataset/dbwalk/ggnn
+data_name=gnn_exception
+lang=python
+
+python -m dbwalk.ggnn.data_util.cook_ast_graphs \
+    -data_dir $data_root \
+    -data $data_name \
+    -language $lang \
+    -data_chunk_size 50000 \
+    $@
