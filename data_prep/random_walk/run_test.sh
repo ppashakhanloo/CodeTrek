6bbe3b4883c26630f5dc1cdad0ad3ee9281c81a1
Yuepeng Wang <yuepeng.ypwang@gmail.com>
2021-04-20 11:46:55 -0500

    Add sample gv and json files

--- data_prep/random_walk/run_test.sh
+++ data_prep/random_walk/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-graph_file=testfiles/file_1_graph.gv
-json_file=testfiles/walks.json
+graph_file=testfiles/train/graph_exception_1.gv
+json_file=testfiles/train/stub_exception_1.json
 
 python3 test.py $graph_file $json_file
