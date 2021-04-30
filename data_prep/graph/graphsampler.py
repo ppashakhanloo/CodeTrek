01e415b82adf45f12a2fcc4b3dcef2a172953a83
Yuepeng Wang <yuepeng.ypwang@gmail.com>
2021-04-30 18:06:17 -0500

    Make node index continuous for sub-graphs

--- data_prep/graph/graphsampler.py
+++ data_prep/graph/graphsampler.py
@@ -1,7 +1,7 @@
 import queue
 from graphviz import Graph
 from pygraphviz import AGraph, Node
-from typing import Set
+from typing import Set, Dict
 
 
 class GraphSampler:
@@ -9,6 +9,8 @@ class GraphSampler:
     @staticmethod
     def sample(agraph: AGraph, anchor_id: str, node_num: int) -> Graph:
         nodes = agraph.nodes()
+        new_id = 1               # type: int
+        name_map = {}            # type: Dict[str, str]
         graph = Graph()
         q = queue.SimpleQueue()  # type: SimpleQueue
         q.put(anchor_id)
@@ -18,7 +20,10 @@ class GraphSampler:
             name = q.get()       # type: str
             if name not in visited:
                 visited.add(name)
-                graph.node(name, nodes[name]['label'])
+                new_name = str(new_id)
+                new_id += 1
+                name_map[name] = new_name
+                graph.node(new_name, nodes[name]['label'])
                 for neighbor in agraph.neighbors(name):
                     if neighbor not in visited:
                         q.put(neighbor)
@@ -27,5 +32,5 @@ class GraphSampler:
             if n1 in visited and n2 in visited:
                 labels = {edge['label'] for edge in list(agraph[n1][n2].values())}
                 for label in labels:
-                    graph.edge(n1, n2, label)
+                    graph.edge(name_map[n1], name_map[n2], label)
         return graph
