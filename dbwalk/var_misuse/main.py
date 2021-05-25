dceea24f26ba716dfd7e993221b6a3c1dcf6db7f
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2021-05-25 13:50:43 -0400

    update varmisuse dbwalk

--- dbwalk/var_misuse/main.py
+++ dbwalk/var_misuse/main.py
@@ -7,34 +7,54 @@ import numpy as np
 import random
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from dbwalk.data_util.dataset import InMemDataest, ProgDict
-from dbwalk.common.configs import cmd_args, set_device
+from dbwalk.data_util.dataset import InMemDataest, ProgDict, FastOnlineWalkDataset
+from dbwalk.common.configs import args, get_torch_device
 from tqdm import tqdm
+import torch.multiprocessing as mp
 
 from dbwalk.model.classifier import BinaryNet
-from dbwalk.training.train import train_loop, binary_eval_dataset
+from dbwalk.training.train import train_loop, binary_eval_dataset, train_mp
 
 
 if __name__ == '__main__':
-    set_device(cmd_args.gpu)
-    np.random.seed(cmd_args.seed)
-    random.seed(cmd_args.seed)
-    torch.manual_seed(cmd_args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    prog_dict = ProgDict(args.data_dir)
+    model = BinaryNet(args, prog_dict)
 
-    prog_dict = ProgDict(cmd_args.data_dir)
-    model = BinaryNet(cmd_args, prog_dict).to(cmd_args.device)
-    db_class = InMemDataest
+    if args.online_walk_gen:
+        db_class = FastOnlineWalkDataset
+    else:
+        db_class = InMemDataest
 
-    if cmd_args.phase == 'test':        
-        assert cmd_args.model_dump is not None
-        model_dump = os.path.join(cmd_args.save_dir, cmd_args.model_dump)
+    if args.phase == 'eval':
+        assert args.model_dump is not None
+        model_dump = os.path.join(args.save_dir, args.model_dump)
         print('loading model from', model_dump)
-        model.load_state_dict(torch.load(model_dump, map_location=cmd_args.device))
-        db_eval = db_class(cmd_args, prog_dict, cmd_args.data_dir, 'eval')
-        eval_loader = db_eval.get_test_loader(cmd_args)
-        binary_eval_dataset(model, 'eval', eval_loader)
+        device = get_torch_device(args.gpu)
+        model = model.to(device)
+        model.load_state_dict(torch.load(model_dump, map_location=device))
+        db_eval = db_class(args, prog_dict, args.data_dir, 'eval')
+        eval_loader = db_eval.get_test_loader(args)
+        binary_eval_dataset(model, 'eval', eval_loader, device)
         sys.exit()
 
-    db_train = db_class(cmd_args, prog_dict, cmd_args.data_dir, 'train', sample_prob=[0.5, 0.5], shuffle_var=cmd_args.shuffle_var)
-    db_dev = db_class(cmd_args, prog_dict, cmd_args.data_dir, 'dev')
-    train_loop(prog_dict, model, db_train, db_dev, binary_eval_dataset)
+    db_train = db_class(args, prog_dict, args.data_dir, 'train', sample_prob=[0.5, 0.5], shuffle_var=args.shuffle_var)
+    db_dev = db_class(args, prog_dict, args.data_dir, 'dev')
+
+    if args.num_train_proc > 1:
+        mp.set_start_method('spawn')
+        if args.gpu_list is not None:
+            devices = [get_torch_device(int(x.strip())) for x in args.gpu_list.split(',')]
+        else:
+            devices = ['cpu'] * args.num_train_proc
+        assert len(devices) == args.num_train_proc
+        procs = []
+        for rank, device in enumerate(devices):
+            proc = mp.Process(target=train_mp, args=(args, rank, device, prog_dict, model, db_train, db_dev, binary_eval_dataset))
+            proc.start()
+        for proc in procs:
+            proc.join()
+    else:
+        train_loop(args, get_torch_device(args.gpu), prog_dict, model, db_train, db_dev, binary_eval_dataset)
