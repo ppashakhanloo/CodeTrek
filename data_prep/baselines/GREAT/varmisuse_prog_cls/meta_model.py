876c166e0061579d2bd8421238da78fd41f0a672
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2021-07-08 02:02:05 -0400

    checkpoint GREAT varmisuse prog cls

---
+++ data_prep/baselines/GREAT/varmisuse_prog_cls/meta_model.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from models import great_transformer, ggnn, rnn, util
+
+class VarmisuseProgModel(tf.keras.layers.Layer):
+  def __init__(self, config, vocab_dim):
+    super(VarmisuseProgModel, self).__init__()
+    self.config = config
+    self.vocab_dim = vocab_dim
+
+  def build(self, _):
+    # These layers are always used; initialize with any given model's hidden_dim
+    random_init = tf.random_normal_initializer(stddev=self.config['base']['hidden_dim'] ** -0.5)
+    self.embed = tf.Variable(random_init([self.vocab_dim, self.config['base']['hidden_dim']]), dtype=tf.float32)
+    self.prediction = tf.keras.layers.Dense(1)
+    self.pos_enc = tf.constant(util.positional_encoding(self.config['base']['hidden_dim'], 5000))
+
+    join_dicts = lambda d1, d2: {**d1, **d2}
+    base_config = self.config['base']
+    desc = self.config['configuration'].split(' ')
+    self.stack = []
+    for kind in desc:
+      if kind == 'rnn':
+        self.stack.append(rnn.RNN(join_dicts(self.config['rnn'], base_config), shared_embedding=self.embed))
+      elif kind == 'ggnn':
+        self.stack.append(ggnn.GGNN(join_dicts(self.config['ggnn'], base_config), shared_embedding=self.embed))
+      elif kind == 'great':
+        self.stack.append(great_transformer.Transformer(join_dicts(self.config['transformer'], base_config), shared_embedding=self.embed))
+      elif kind == 'transformer':
+        joint_config = join_dicts(self.config['transformer'], base_config)
+        joint_config['num_edge_types'] = None
+        self.stack.append(great_transformer.Transformer(joint_config, shared_embedding=self.embed))
+      else:
+        raise ValueError('Unknown model component provided:', kind)
+
+  def call(self, tokens, token_mask, edges, training):
+    subtoken_embeddings = tf.nn.embedding_lookup(self.embed, tokens)
+    subtoken_embeddings *= tf.expand_dims(tf.cast(tf.clip_by_value(tokens, 0, 1), dtype='float32'), -1)
+    states = tf.reduce_mean(subtoken_embeddings, 2)
+    if not self.stack or not isinstance(self.stack[0], rnn.RNN):
+      states += self.pos_enc[:tf.shape(states)[1]]
+
+    for model in self.stack:
+      if isinstance(model, great_transformer.Transformer):
+        mask = tf.cast(token_mask, dtype='float32')
+        mask = tf.expand_dims(tf.expand_dims(mask, 1), 1)
+        attention_bias = tf.stack([edges[:, 0], edges[:, 1], edges[:, 3], edges[:, 2]], axis=1)
+        states = model(states, mask, attention_bias, training=training)
+      else:
+        raise ValueError('Model not yet supported:', model)
+
+    return tf.transpose(self.prediction(states), [0, 2, 1])
+
+  def get_loss(self, logits, token_mask, labels):
+    seq_mask = tf.cast(token_mask, 'float32')
+    logits += (1.0 - tf.expand_dims(seq_mask, 1)) * tf.float32.min
+    probs = tf.sigmoid(logits[:,0][:,0])
+
+    labels = tf.cast(labels, 'float32')
+    loss = -labels * tf.math.log(probs + 1e-9) - (1.0 - labels) * tf.math.log(1.0 - probs + 1e-9)
+    loss = tf.reduce_mean(loss)
+
+    pred_labels = tf.round(probs)
+    acc = tf.reduce_mean(tf.cast(tf.math.equal(pred_labels, labels), 'float32'))
+    auc = tf.keras.metrics.AUC()
+    auc.update_state(tf.cast(labels, 'float32'), probs)
+    return loss, acc, auc.result().numpy()
+
+  def run_dummy_input(self):
+    self(tf.ones((3,3,10), dtype='int32'), tf.ones((3,3), dtype='int32'), tf.ones((2,4), dtype='int32'), True)
---
+++ data_prep/baselines/GREAT/varmisuse_prog_cls/run_model.py
@@ -0,0 +1,110 @@
+import sys
+sys.path.append('.')
+
+import argparse
+import yaml
+
+import numpy as np
+import tensorflow as tf
+
+from checkpoint_tracker import Tracker
+from data import data_loader_varmisuse_prog, vocabulary
+from meta_model import VarmisuseProgModel
+
+def main():
+  ap = argparse.ArgumentParser()
+  ap.add_argument("data_path", help="Path to data root")
+  ap.add_argument("vocabulary_path", help="Path to vocabulary file")
+  ap.add_argument("config", help="Path to config file")
+  ap.add_argument("-m", "--models", help="Directory to store trained models (optional)")
+  ap.add_argument("-l", "--log", help="Path to store training log (optional)")
+  ap.add_argument("-e", "--eval_only", help="Whether to run just the final model evaluation")
+  args = ap.parse_args()
+  config = yaml.safe_load(open(args.config))
+  print("Training with configuration:", config)
+  data = data_loader_varmisuse_prog.DataLoader(args.data_path, config["data"], vocabulary.Vocabulary(args.vocabulary_path))
+  if args.eval_only:
+    if args.models is None or args.log is None:
+      raise ValueError("Must provide a path to pre-trained models when running final evaluation")
+    test(data, config, args.models, args.log)
+  else:
+    train(data, config, args.models, args.log)
+
+def test(data, config, model_path, log_path):
+  model = VarmisuseProgModel(config['model'], data.vocabulary.vocab_dim)
+  model.run_dummy_input()
+  tracker = Tracker(model, model_path, log_path)
+  tracker.restore(best_model=True)
+  acc, auc = evaluate(data, config, model, is_heldout=False)
+  print("Final Results: acc: {0:.2%}, auc: {1:.2%}".format(acc, auc))
+
+def train(data, config, model_path=None, log_path=None):
+  model = VarmisuseProgModel(config['model'], data.vocabulary.vocab_dim)
+  model.run_dummy_input()
+  print("Model initialized, training {:,} parameters".format(np.sum([np.prod(v.shape) for v in model.trainable_variables])))
+  optimizer = tf.optimizers.Adam(config["training"]["learning_rate"])
+
+  # Restore model from checkpoints if present; also sets up logger
+  if model_path is None:
+    tracker = Tracker(model)
+  else:
+    tracker = Tracker(model, model_path, log_path)
+  tracker.restore()
+  if tracker.ckpt.step.numpy() > 0:
+    print("Restored from step:", tracker.ckpt.step.numpy() + 1)
+  else:
+    print("Step:", tracker.ckpt.step.numpy() + 1)
+
+  counter = 0
+  loss, acc, auc = get_metrics()
+  while tracker.ckpt.step < config["training"]["max_steps"]:
+    for batch in data.batcher(mode='train'):
+      counter += 1
+      tokens, edges, label, _, _ = batch
+      token_mask = tf.clip_by_value(tf.reduce_sum(tokens, -1), 0, 1)
+
+      with tf.GradientTape() as tape:
+        preds = model(tokens, token_mask, edges, training=True)
+        ls, acs, aus = model.get_loss(preds, token_mask, label)
+        update_metrics(loss, acc, auc, ls, acs, aus)
+
+      grads = tape.gradient(ls, model.trainable_variables)
+      grads, _ = tf.clip_by_global_norm(grads, 0.25)
+      optimizer.apply_gradients(list(zip(grads, model.trainable_variables)))
+
+      samples = tf.shape(token_mask)[0]
+      prev_samples = tracker.get_samples()
+      curr_samples = tracker.update_samples(samples)
+      if counter % config['training']['print_freq'] == 0:
+        print("Progress: loss: {0:.3f}, acc: {1:.2%}, auc: {2:.2%}".format(ls, acs, aus))
+
+      if counter % config['data']['valid_interval'] == 0:
+        avg_accs = evaluate(data, config, model)
+        tracker.save_checkpoint(model, avg_accs)
+        print("Saving the best model...")
+
+  print("Final Results: loss: {0:.3f}, acc: {1:.2%}, auc: {2:.2%}".format(loss.numpy(), acc.numpy(), auc.numpy()))
+
+def evaluate(data, config, model, is_heldout=True):  # Similar to train, just without gradient updates
+  loss, acc, auc = get_metrics()
+  for batch in data.batcher(mode='dev' if is_heldout else 'eval'):
+    tokens, edges, label, _, _ = batch
+    token_mask = tf.clip_by_value(tf.reduce_sum(tokens, -1), 0, 1)
+    preds = model(tokens, token_mask, edges, training=False)
+    ls, acs, aus = model.get_loss(preds, token_mask, label)
+    update_metrics(loss, acc, auc, ls, acs, aus)
+  return acc.result().numpy(), auc.result().numpy()
+
+def get_metrics():
+  loss = tf.keras.metrics.Mean()
+  acc = tf.keras.metrics.Mean()
+  auc = tf.keras.metrics.Mean()
+  return loss, acc, auc
+
+def update_metrics(loss, acc, auc, ls, acs, aus):
+  loss.update_state(ls)
+  acc.update_state(acs)
+  auc.update_state(aus)
+
+if __name__ == '__main__':
+  main()
